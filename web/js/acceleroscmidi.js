b7d55396eaa360e0802eadf1b36caec7d7d74753
jeffyactive <jeff@reelyactive.com>
2017-06-29 18:56:12 -0400

    First sound control

--- web/js/acceleroscmidi.js
+++ web/js/acceleroscmidi.js
@@ -10,8 +10,14 @@ angular.module('acceleroscmidi', [ 'ui.bootstrap' ])
               $location.port();
     var socket = io.connect(url);
 
+    var pan = new Tone.Panner(0).toMaster();
+    var effect = new Tone.Chorus(60, 2.5, 0.1).connect(pan);
+    var synth = new Tone.AMOscillator("C3", "sine", "sine").connect(effect).start();
+
     socket.on('accelerometer', function(accelerometer) {
       $scope.accelerometer = accelerometer;
+      pan.pan.value = accelerometer.angX;
+      effect.frequency.value = (accelerometer.angY + 1) * 4;
       $scope.$apply();
     });
   });
